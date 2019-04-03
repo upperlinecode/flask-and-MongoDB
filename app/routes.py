2a13a0e51bdba20cc5db1628ade4e70904d2c22d
Jeffrey Lancaster <jeffrey.lancaster@gmail.com>
2019-04-03 16:02:29 -0400

    small tweaks

--- app/routes.py
+++ app/routes.py
@@ -2,10 +2,9 @@ import os
 from app import app
 from flask import render_template, request, redirect
 
-
-events = [{"event_name":"Guitar Recital", "event_date":"4/15/2019"},
-    {"event_name":"Poetry Slam Competition", "event_date":"5/4/2019"},
-    {"event_name":"Community Board Meeting", "event_date":"6/23/2019"}]
+events = [{"event_name":"Guitar Recital", "event_date":"2019-15-04"},
+    {"event_name":"Poetry Slam Competition", "event_date":"2019-04-05"},
+    {"event_name":"Community Board Meeting", "event_date":"2019-23-06"}]
 
 @app.route('/')
 @app.route('/index')
@@ -21,7 +20,7 @@ def new_event():
     if request.method == "GET":
         return render_template('new_event.html')
     else:
-        # new_event = dict(request.form.to_dict())
+        # new_event = dict(request.form)
         new_event = request.form.to_dict()
         events.append(new_event)
         return redirect('/')
