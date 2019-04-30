17b14fee443fd37c528798c4297a22ae32e85827
Jeffrey Lancaster <jeffrey.lancaster@gmail.com>
2019-04-30 18:12:11 +0000

    minimizing repo

--- app/routes.py
+++ app/routes.py
@@ -1,15 +1,23 @@
 import os
 from app import app
 from flask import render_template, request, redirect
-from flask_pymongo import PyMongo
+
+events = [
+        {"event":"First Day of Classes", "date":"2019-08-21"},
+        {"event":"Winter Break", "date":"2019-12-20"},
+        {"event":"Finals Begin", "date":"2019-12-01"}
+    ]
+
+
+# from flask_pymongo import PyMongo
 
 # name of database
-app.config['MONGO_DBNAME'] = 'database-name' 
+# app.config['MONGO_DBNAME'] = 'database-name' 
 
 # URI of database
-app.config['MONGO_URI'] = 'mongo-uri' 
+# app.config['MONGO_URI'] = 'mongo-uri' 
 
-mongo = PyMongo(app)
+# mongo = PyMongo(app)
 
 
 # INDEX
@@ -18,7 +26,7 @@ mongo = PyMongo(app)
 @app.route('/index')
 
 def index():
-    return "A Community Events Board"
+    return render_template('index.html', events = events)
 
 
 # CONNECT TO DB, ADD DATA
@@ -26,36 +34,9 @@ def index():
 @app.route('/add')
 
 def add():
-    user = mongo.db.users
-    user.insert({'name':'Your Name'})
-    return 'Added User!'
+    # connect to the database
 
+    # insert new data
 
-# ADD NEW EVENT VIA FORM
-
-@app.route('/events/new', methods=['GET', 'POST'])
-
-def new_event():
-    if request.method == "GET":
-        return render_template('new_event.html')
-    else:
-        user_name = request.form['user_name']
-        event_name = request.form['event_name']
-        event_date = request.form['event_date']
-
-        events = mongo.db.events
-        events.insert({'event': event_name, 'date': event_date, 'user': user_name})
-        return redirect('/')
-
-
-# SHOW ALL EVENTS IN DATABASE
-
-@app.route('/events')
-
-def events():
-    collection = mongo.db.events
-    events = collection.find({})
-
-    return render_template('events.html', events = events)
-
-
+    # return a message to the user
+    return ""
